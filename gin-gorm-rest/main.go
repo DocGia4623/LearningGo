7437c88c8a5f67de662be3f9c5fcbb5d095faa6c
OneMind-Dev <anhkvse182347@fpt.edu.vn>
2025-01-13 11:44:46 +0700

    middlewares, library

--- gin-gorm-rest/main.go
+++ gin-gorm-rest/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"io"
+	"log"
+	"os"
 	"vietanh/gin-gorm-rest/config"
+	"vietanh/gin-gorm-rest/middlewares"
 	"vietanh/gin-gorm-rest/routes"
 
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
+	setupLogOutput()
 	router := gin.New()
+	log.Println("Starting server...")
+	router.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 	config.Connect()
 	routes.UserRoute(router)
 	router.Run(":8080")
