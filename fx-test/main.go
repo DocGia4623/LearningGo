db6174c16bba193e7dfe794a5790c3344121a2a2
OneMind-Dev <anhkvse182347@fpt.edu.vn>
2025-02-17 14:22:40 +0700

    test fx

---
+++ fx-test/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+
+	"go.uber.org/fx"
+)
+
+func main() {
+	// t := Title("string")
+	// p := NewPublisher(&t)
+	// m := NewMainService(p)
+	// m.Run()
+	fx.New(
+		fx.Provide(NewMainService),
+		fx.Provide(
+			fx.Annotate(
+				NewPublisher,
+				fx.As(new(IPublisher)),
+				fx.ParamTags(`group:"titles"`),
+			),
+		),
+		fx.Provide(
+			titleComponent("goobye"),
+		),
+
+		fx.Provide(
+			titleComponent("hello"),
+		),
+		fx.Provide(
+			titleComponent("world"),
+		),
+		fx.Invoke(func(service *MainService) {
+			service.Run()
+		}),
+	).Run()
+
+}
+
+func titleComponent(title string) any {
+	return fx.Annotate(
+		func() *Title {
+			t := Title(title)
+			return &t
+		},
+		fx.ResultTags(`group:"titles"`),
+	)
+}
+
+// Main service
+type MainService struct {
+	publisher IPublisher
+}
+
+func NewMainService(publisher IPublisher) *MainService {
+	return &MainService{publisher: publisher}
+}
+
+func (service *MainService) Run() {
+	service.publisher.Publish()
+	log.Println("main program")
+}
+
+// Dependency
+type IPublisher interface {
+	Publish()
+}
+
+type Publisher struct {
+	title []*Title
+}
+
+func NewPublisher(titles ...*Title) *Publisher {
+	return &Publisher{title: titles}
+}
+
+func (publisher *Publisher) Publish() {
+	for _, title := range publisher.title {
+		log.Println("publishing", *title)
+	}
+}
+
+// Dependency of publisher
+
+type Title string
